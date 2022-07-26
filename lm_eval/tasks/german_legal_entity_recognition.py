9c7a006181c66f1c0554c7fd298bf52b6b8d9e75
karina-hensel <henselkarina@gmail.com>
2022-07-26 12:46:55 +0200

    Prediction updated

--- lm_eval/tasks/german_legal_entity_recognition.py
+++ lm_eval/tasks/german_legal_entity_recognition.py
@@ -56,8 +56,8 @@ def _german_ler_agg_f1(key, items):
 
 class GermanLegalEntityRecognition(Task):
     VERSION = 0
-    DATASET_PATH = 'jfrenz/legalglue'
-    DATASET_NAME = None#'german_ler'
+    DATASET_PATH = 'legalglue'
+    DATASET_NAME = 'german_ler'
 
     def has_training_docs(self):
         return True
