52d425d7e76af5ea3f28f5aa15de9bbbecac6f66
karina-hensel <henselkarina@gmail.com>
2022-07-27 06:17:56 +0200

    germaner dataset

--- lm_eval/tasks/german_legal_entity_recognition.py
+++ lm_eval/tasks/german_legal_entity_recognition.py
@@ -56,7 +56,7 @@ def _german_ler_agg_f1(key, items):
 
 class GermanLegalEntityRecognition(Task):
     VERSION = 0
-    DATASET_PATH = 'german_ner'
+    DATASET_PATH = 'germaner'
     DATASET_NAME = None
 
     def has_training_docs(self):
